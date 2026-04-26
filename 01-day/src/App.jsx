81243bc546b6dc29589136f8db5a96ebf1669da9
Satyamkeshari <satyamkumarkeshari358@gmail.com>
2026-04-26 22:29:15 +0530

    day 1

---
+++ 01-day/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+ 
+
+  return (
+    <>
+      <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+        <h1 className="text-3xl text-blue-500 bg-gray-200 px-4 py-2 rounded">
+          Hello Tailwind
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App

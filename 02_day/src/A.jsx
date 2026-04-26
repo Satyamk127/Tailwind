4cbc44b11147bf4790c89331445999ac9ecc5f68
Satyamkeshari <satyamkumarkeshari358@gmail.com>
2026-04-26 22:56:07 +0530

    day 1

---
+++ 02_day/src/A.jsx
@@ -0,0 +1,17 @@
+
+function A() {
+
+
+  return (
+    <>
+      <div class="p-6 m-6 bg-blue-100 shadow-lg">
+        <h2 class="text-xl font-bold">Title</h2>
+        <p class="text-gray-500">Description</p>
+      </div>
+
+
+    </>
+  )
+}
+
+export default A
---
+++ 02_day/src/Nav.jsx
@@ -0,0 +1,17 @@
+
+
+function Nav() {
+
+
+  return (
+    <>
+      <h1 className="text-red-300 text-2xl" >Heading 1</h1>
+      <h2 className=" text-2xl text-red-400">Heading 2</h2>
+      <h3 className="text-3xl text-red-700">Heading 3</h3>
+
+
+    </>
+  )
+}
+
+export default Nav

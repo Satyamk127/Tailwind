3c5fdd8d0d2c5a59a8b7fabd44194d564929c213
Satyamkeshari <satyamkumarkeshari358@gmail.com>
2026-04-28 15:37:12 +0530

    commnet

--- 04-day/src/App.jsx
+++ 04-day/src/App.jsx
@@ -4,12 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
-    <h1 className='bg-red-400'>hello incapp</h1>
-      
+
+      <div className='flex  gap-20 justify-center  items-center h-100 p-30' >
+        <div className='flex bg-sky-400 w-70 p-5 flex-col text-white  items-center'>
+          <img className='h-50 border rounded-sm' src="https://images.unsplash.com/photo-1761839258044-e59f324b5a7f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8" alt="" />
+          <p>image 1</p>
+          <p>discription</p>
+
+        </div>
+        <div className='flex bg-sky-400 w-70 p-5 flex-col text-white  items-center'>
+
+          <img className='h-50  border rounded-sm' src="https://images.unsplash.com/photo-1776549821469-e8aab812dba0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzMXx8fGVufDB8fHx8fA%3D%3D" alt="" />
+           <p>image 2</p>
+          <p>discription</p>
+        </div>
+        <div className='flex bg-sky-400 w-70 p-5 flex-col text-white  items-center'>
+          <img className='h-50 border rounded-sm' src="https://images.unsplash.com/photo-1761839257864-c6ccab7238de?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D" alt="" />
+           <p>image 3</p>
+          <p>discription</p>
+        </div>
+      </div>
+
     </>
   )
 }

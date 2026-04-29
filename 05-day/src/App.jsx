dec82695a0f04a278e6ed6041d0e9d68991a6829
Satyamkeshari <satyamkumarkeshari358@gmail.com>
2026-04-29 14:02:13 +0530

    Grid

---
+++ 05-day/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Grid from './Grid'
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='bg-red-100 '>Today I Learn what is the Grid </h1>
+    
+    <Grid/>
+
+     
+     
+    </>
+  )
+}
+
+export default App
---
+++ 05-day/src/Grid.jsx
@@ -0,0 +1,26 @@
+
+
+function Grid() {
+
+
+  return (
+    <>
+      <h1 className='bg-red-100 '>Today I Learn what is the Grid </h1>
+      <div className='grid grid-cols-3 gap-10 '>
+        <div className='bg-red-600 p-4 '>Item 1</div>
+        <div className='bg-red-600 p-4 '>Item 2</div>
+        <div className='bg-red-600 p-4 '>Item 3</div>
+        <div class="bg-blue-200 p-4">1</div>
+        <div class="bg-blue-200 p-4">2</div>
+        <div class="bg-blue-200 p-4">3</div>
+        <div class="bg-blue-200 p-4">4</div>
+        <div class="bg-blue-200 p-4">5</div>
+      </div>
+
+
+
+    </>
+  )
+}
+
+export default Grid

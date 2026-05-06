245021f9f630f474e56b65ef6992faaa91d8bd4b
Satyamkeshari <satyamkumarkeshari358@gmail.com>
2026-05-06 16:56:53 +0530

    commet

---
+++ 10-day/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex justify-between p-6 items-center text-black'>
+        <div className='flex gap-5'>
+          <div>WIX</div>
+          <div>Prduct</div>
+          <div>Solution</div>
+          <div>Resources</div>
+          <div>Price</div>
+          <div>Wix Studio</div>
+          <div>Enterprise</div></div>
+
+        <div className='flex gap-5'>
+          <div>Loing</div>
+          <div><button>Get Started</button></div>
+        </div>
+      </div>
+
+
+    </>
+  )
+}
+
+export default App

83747fede348f140dee9d53d191385985c3edec9
Satyamkeshari <satyamkumarkeshari358@gmail.com>
2026-05-12 20:54:53 +0530

    paragrap change

--- 10-day/src/Mid.jsx
+++ 10-day/src/Mid.jsx
@@ -5,13 +5,13 @@ function Mid() {
       <div className="flex justify-between items-center h-120  text-white bg-sky-600 ">
         <div className="bg-red-600 h-75 ml-10   p-5 rounded-4xl flex flex-col justify-center items-center">
           <img className="h-60 rounded-2xl"  src="https://media.istockphoto.com/id/1200486875/photo/different-computers-in-store-background.jpg?s=612x612&w=0&k=20&c=KUkYqOE9DBzdDfWyX5NoVNzVnAOrlGzf8uUi8GNMc-4=" alt="" />
-          <p>This is Given first project</p></div>
+          <p className="mt-2">This is Given first project</p></div>
         <div className="bg-red-600 h-75 ml-10   p-5 rounded-4xl flex flex-col justify-center items-center">
           <img className="h-60 rounded-2xl"  src="https://media.istockphoto.com/id/1200486875/photo/different-computers-in-store-background.jpg?s=612x612&w=0&k=20&c=KUkYqOE9DBzdDfWyX5NoVNzVnAOrlGzf8uUi8GNMc-4=" alt="" />
-          <p>This is Given first project</p></div>
+          <p className="mt-2">This is Given first project</p></div>
           <div className="bg-red-600 h-75 mr-10   p-5 rounded-4xl flex flex-col justify-center items-center">
           <img className="h-60 rounded-2xl"  src="https://media.istockphoto.com/id/1200486875/photo/different-computers-in-store-background.jpg?s=612x612&w=0&k=20&c=KUkYqOE9DBzdDfWyX5NoVNzVnAOrlGzf8uUi8GNMc-4=" alt="" />
-          <p>This is Given first project</p></div>
+          <p className="mt-2">This is Given first project</p></div>
       </div>
     </>
   );

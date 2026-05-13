c78106f7728bcd96e8a73d649dba22128c16e95e
Satyamkeshari <satyamkumarkeshari358@gmail.com>
2026-05-13 21:08:14 +0530

    update footer

--- 10-day/src/Footer.jsx
+++ 10-day/src/Footer.jsx
@@ -1,7 +1,20 @@
 function Footer(){
   return(
     <>
-     <p className="bg-black text-white  text-center p-10"><span className="bg-red-700 p-2 rounded-2xl">© 2026 Satyam Keshari. All Rights Reserved.</span></p>
+    
+
+     <div className="bg-black text-white  text-center h-40 ">
+      <form   className="flex flex-col h-30" action="">
+        <input className="text-white" type="text"  placeholder="Enter your first name "/>
+        <input type="text" placeholder="Enter your last name " />
+        <input type="text" placeholder="Enter your  Email "/>
+        <input type="text" placeholder="Enter your password " />
+       <button>Submit</button>
+      </form>
+
+      © 2026 Satyam Keshari. All Rights Reserved.
+      
+     </div>
     </>
   );
 }

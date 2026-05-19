a049bc4613fc20c5821489f57f2e3f8b8c3cb45d
Satyamkeshari <satyamkumarkeshari358@gmail.com>
2026-05-19 21:33:56 +0530

    update blog

---
+++ 11-day/src/data/blogs.js
@@ -0,0 +1,21 @@
+const blogs = [
+
+  {
+    id:1,
+    title:"Master React Like a Pro",
+    description:"Learn advanced React concepts with modern architecture.",
+    image:"https://images.unsplash.com/photo-1498050108023-c5249f4df085",
+    likes:120,
+  },
+
+  {
+    id:2,
+    title:"Tailwind CSS Best Practices",
+    description:"Build premium UI using Tailwind CSS.",
+    image:"https://images.unsplash.com/photo-1515879218367-8466d910aaa4",
+    likes:95,
+  },
+
+]
+
+export default blogs
